d31374390b213e9a11431e401489752d1a35118e
Benjamin Schubert <Ben@benjamins-mbp.torolab.ibm.com>
2016-05-20 16:31:11 -0400

    Skeleton for colour encode and decode done. Decode returns weird results. Not sure if it's because of it's encoding badly or decoding badly.

--- steg.py
+++ steg.py
@@ -109,6 +109,27 @@ class App:
 		currentHeight = self.currentImage.size[1]
 		currentPix = self.currentImage.load()
 
+		numBits = int(self.numBitsBox.get())
+		exp = 8 - min(8,numBits*3)
+		factor = 2**exp
+
+		# for 1 bits: 1111 1110
+		# for 2 bits: 1111 1100
+		# for 3 bits: 1111 1000
+		baseMask = (255 ^ (2**numBits - 1))
+
+
+		#   -----------------------------------------
+		# |       |   1-bit   |   2-bit   |   3-bit   |
+		# | ----- | --------- | --------- | --------- |
+		# | maskR | 0000 0100 | 0011 0000 | 1100 0000 |
+		# | maskG | 0000 0010 | 0000 1100 | 0011 1000 |
+		# | maskB | 0000 0001 | 0000 0011 | 0000 0111 |
+		#   -----------------------------------------
+		targetMaskR = (2**numBits - 1) << 2 * numBits
+		targetMaskG = (2**numBits - 1) << numBits
+		targetMaskB = 2**numBits - 1
+
 
 		# grayscale encoding -- MUCH more simple than colour
 		if self.useColour.get() == 0:
@@ -122,26 +143,7 @@ class App:
 			x0 = currentWidth/2 - secretWidth/2
 			y0 = currentHeight/2 - secretHeight/2
 
-			numBits = int(self.numBitsBox.get())
-			exp = 8 - min(8,numBits*3)
-			factor = 2**exp
-
-			# for 1 bits: 1111 1110
-			# for 2 bits: 1111 1100
-			# for 3 bits: 1111 1000
-			baseMask = (255 ^ (2**numBits - 1))
-
-
-			#   -----------------------------------------
-			# |       |   1-bit   |   2-bit   |   3-bit   |
-			# | ----- | --------- | --------- | --------- |
-			# | maskR | 0000 0100 | 0011 0000 | 1100 0000 |
-			# | maskG | 0000 0010 | 0000 1100 | 0011 1000 |
-			# | maskB | 0000 0001 | 0000 0011 | 0000 0111 |
-			#   -----------------------------------------
-			targetMaskR = (2**numBits - 1) << 2 * numBits
-			targetMaskG = (2**numBits - 1) << numBits
-			targetMaskB = 2**numBits - 1
+			
 
 			print "Encoding..."
 
@@ -176,7 +178,7 @@ class App:
 		elif self.useColour.get() == 1:
 			print "Using colour..."
 
-			# This should be a multiple of 3 or weird things will happen below
+			# This should be a multiple of 3, or weird things will happen below
 			SIZE_BITS = 12
 
 			widthBits = [None] * SIZE_BITS
@@ -194,9 +196,9 @@ class App:
 				widthMask[i] = 254 | widthBits[i]
 				heightMask[i] = 254 | heightBits[i]
 
-			pix = currentPix[0,0]
-			currentPix[0,0] = (pix[0] & widthMask[0] | widthBits[0], pix[1] & widthMask[1] | widthBits[1],\
-								pix[2] & widthMask[2] | widthBits[2])
+			#pix = currentPix[0,0]
+			#currentPix[0,0] = (pix[0] & widthMask[0] | widthBits[0], pix[1] & widthMask[1] | widthBits[1],\
+			#					pix[2] & widthMask[2] | widthBits[2])
 
 			# Encode size data in public image
 			for i in range(0, SIZE_BITS/3):
@@ -207,14 +209,73 @@ class App:
 
 				pix = currentPix[SIZE_BITS/3 + i, 0]
 				currentPix[SIZE_BITS/3 + i, 0] = (pix[0] & heightMask[3*i]     | heightBits[3*i],\
-									  pix[1] & heightMask[3*i + 1] | heightBits[3*i + 1],\
-									  pix[2] & heightMask[3*i + 2] | heightBits[3*i + 2])
+						pix[1] & heightMask[3*i + 1] | heightBits[3*i + 1],\
+						pix[2] & heightMask[3*i + 2] | heightBits[3*i + 2])
 
 
 			# Pixels 0 through 7 now contain 12-bit dimensions data.
 			
-			
+			rgbIndex = 0
 
+
+			# First line
+			# Keeping this separate because of the offset of 8
+			for i in range(8,currentWidth):
+				
+				targetValue = int(secretPix[int(rgbIndex/3)%secretWidth, int(int(rgbIndex/3)/secretWidth)][rgbIndex%3] / factor)
+				
+				shiftedTargetR = (targetValue & targetMaskR) >> (2 * numBits)
+				shiftedTargetG = (targetValue & targetMaskG) >> numBits
+				shiftedTargetB = targetValue & targetMaskB
+
+				r = baseMask | shiftedTargetR
+				g = baseMask | shiftedTargetG
+				b = baseMask | shiftedTargetB
+
+				oldPixel = currentPix[i, 0]
+
+				# Can't forget both & and | here, since we want to both set and unset the proper bits
+				pixel = (oldPixel[0] & r | shiftedTargetR, oldPixel[1] & g | shiftedTargetG,\
+						 oldPixel[2] & b | shiftedTargetB)
+				currentPix[i, 0] = pixel
+
+				rgbIndex += 1
+
+				if int(rgbIndex/3) >= secretWidth * secretHeight:
+					break
+
+
+			# Same as above, starting with 0
+			for x in range(0,currentWidth):
+				for y in range(1,currentHeight):
+				
+					targetValue = int(secretPix[int(rgbIndex/3)%secretWidth, int(int(rgbIndex/3)/secretWidth)][rgbIndex%3] / factor)
+					
+					shiftedTargetR = (targetValue & targetMaskR) >> (2 * numBits)
+					shiftedTargetG = (targetValue & targetMaskG) >> numBits
+					shiftedTargetB = targetValue & targetMaskB
+
+					r = baseMask | shiftedTargetR
+					g = baseMask | shiftedTargetG
+					b = baseMask | shiftedTargetB
+
+					oldPixel = currentPix[x, y]
+
+					# Can't forget both & and | here, since we want to both set and unset the proper bits
+					pixel = (oldPixel[0] & r | shiftedTargetR, oldPixel[1] & g | shiftedTargetG,\
+							 oldPixel[2] & b | shiftedTargetB)
+					currentPix[x, y] = pixel
+
+					rgbIndex += 1
+
+					if int(rgbIndex/3) >= secretWidth * secretHeight:
+						break
+
+				if int(rgbIndex/3) >= secretWidth * secretHeight:
+					break
+
+			print "Encoding is done?!"
+			print "rgbIndex is {}".format(rgbIndex)
 			#print widthBits
 			#print heightBits
 
@@ -235,9 +296,9 @@ class App:
 
 
 	def decodeImage(self):
-		w = self.currentImage.size[0]
-		h = self.currentImage.size[1]
 
+		currentWidth = self.currentImage.size[0]
+		currentHeight = self.currentImage.size[1]
 		pixels = self.currentImage.load()
 
 		numBits = int(self.numBitsBox.get())
@@ -245,20 +306,75 @@ class App:
 		factor = 2**exp
 
 
-		for x in range(0,w):
-			for y in range(0,h):
-				pix = pixels[x,y]
-				pre = ((pix[0] & (2**numBits-1)) << 2*numBits) + \
-				      ((pix[1] & (2**numBits-1)) << numBits) + \
-				       (pix[2] & (2**numBits-1))
-				
-				gray = pre * factor
-				pixels[x,y] = (gray, gray, gray)
+
+		if self.useColour.get() == 0:
+			w = self.currentImage.size[0]
+			h = self.currentImage.size[1]
 
 
-		tkimage = ImageTk.PhotoImage(self.currentImage)
-		self.imageLabel.configure(image=tkimage)
-		self.imageLabel.image = tkimage
+			for x in range(0,w):
+				for y in range(0,h):
+					pix = pixels[x,y]
+					pre = ((pix[0] & (2**numBits-1)) << 2*numBits) + \
+					      ((pix[1] & (2**numBits-1)) << numBits) + \
+					       (pix[2] & (2**numBits-1))
+					
+					gray = pre * factor
+					pixels[x,y] = (gray, gray, gray)
+
+			tkimage = ImageTk.PhotoImage(self.currentImage)
+			self.imageLabel.configure(image=tkimage)
+			self.imageLabel.image = tkimage
+
+
+
+		elif self.useColour.get() == 1:
+
+			secretWidth = 0
+			secretHeight = 0
+
+			SIZE_BITS = 12
+
+			# Get dimensions
+			for i in range(0, SIZE_BITS):
+				secretWidth += (pixels[int(i/3), 0][i%3] & 1) << (SIZE_BITS - 1 - i)
+				secretHeight += (pixels[int( (SIZE_BITS+i)/3 ), 0][i%3] & 1) << (SIZE_BITS - 1 - i)
+
+			secretImage = Image.new("RGB", (secretWidth,secretHeight))
+			secretPix = secretImage.load()
+
+			secretIndex = 0
+
+			# Iterate through current pixels
+			index = 8
+			while index < currentWidth * currentHeight and secretIndex < secretWidth * secretHeight:
+
+				r = (pixels[index%currentWidth, int(index/currentWidth)][0] & 1) << 2
+				r += (pixels[index%currentWidth, int(index/currentWidth)][1] & 1) << 1
+				r += (pixels[index%currentWidth, int(index/currentWidth)][2] & 1)
+				r = r * factor
+
+				g = (pixels[(index+1)%currentWidth, int((index+1)/currentWidth)][0] & 1) << 2
+				g += (pixels[(index+1)%currentWidth, int((index+1)/currentWidth)][1] & 1) << 1
+				g += (pixels[(index+1)%currentWidth, int((index+1)/currentWidth)][2] & 1)
+				g = g * factor
+
+				b = (pixels[(index+2)%currentWidth, int((index+2)/currentWidth)][0] & 1) << 2
+				b += (pixels[(index+2)%currentWidth, int((index+2)/currentWidth)][1] & 1) << 1
+				b += (pixels[(index+2)%currentWidth, int((index+2)/currentWidth)][2] & 1)
+				b = b * factor
+
+				secretPix[secretIndex%secretWidth, int(secretIndex/secretWidth)] = (r,g,b)
+
+				index += 3
+				secretIndex += 1
+
+
+			tkimage = ImageTk.PhotoImage(secretImage)
+			self.imageLabel.configure(image=tkimage)
+			self.imageLabel.image = tkimage
+
+		
 
 		print "Done decoding."
 
